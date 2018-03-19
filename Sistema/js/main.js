95625ce365d991f8acf1fe5d6a34808e82791cc1
Olmo Zavala <olmozavala@gmail.com>
2018-03-18 21:53:13 -0400

    Displaying some maps already

--- Sistema/js/main.js
+++ Sistema/js/main.js
@@ -1,72 +1,104 @@
+var svg; 
+var margin = 10;
+var projection;
+var path;
+var data;
+var dataGeo = {};
+
 (function(){
-
-  var height = 600;
-  var width = 900;
-  var projection = d3.geo.mercator();
-  var mexico = void 0;
-  var db = d3.map();
-  var sparkline = d3.charts.sparkline().height(50).width(138);
-
-  var setDb = function(data) {
-    data.forEach(function(d) {
-      db.set(d.geoID, [
-        {"x": 1, "y": +d.q1},
-        {"x": 2, "y": +d.q2},
-        {"x": 3, "y": +d.q3},
-        {"x": 4, "y": +d.q4}]);
-    });
-  };
-
-  var geoID = function(d) {
-    return "c" + d.properties.ID_1;
-  };
-
-  var hover = function(d) {
-    var div = document.getElementById('tooltip');
-    div.style.left = event.pageX +'px';
-    div.style.top = event.pageY + 'px';
-    div.innerHTML = d.properties.NAME_1;
-
-    var id = geoID(d);
-    d3.select("#tooltip").datum(db.get(id)).call(sparkline.draw);
-  };
-
-  var path = d3.geo.path().projection(projection);
-
-  var svg = d3.select("#map")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
-
-  d3.json('Datos/MapsGeoJson/Deportivas.json', function(data) {
-	  var x = 1;
-  });
-
-  d3.json('geo-data.json', function(data) {
-    var states = topojson.feature(data, data.objects.MEX_adm1);
-
-    var b, s, t;
-    projection.scale(1).translate([0, 0]);
-    var b = path.bounds(states);
-    var s = .9 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
-    var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-    projection.scale(s).translate(t);
-
-    var map = svg.append('g').attr('class', 'boundary');
-    mexico = map.selectAll('path').data(states.features);
-
-    mexico.enter()
-       .append('path')
-       .attr('d', path)
-       .on("mouseover", hover);
-
-    mexico.attr('fill', '#eee');
-
-    mexico.exit().remove();
-
-    d3.csv('states-data.csv', function(data) {
-      setDb(data);
-    });
-  });
-
+	
+	var width = window.innerWidth - 2*margin;
+	var height= window.innerHeight - 2*margin;
+	projection = d3.geo.mercator()
+			.scale(2299000)
+	// Center the Map in Mexico City
+			.center([-99.1772, 19.330])
+			.translate([width / 2, height / 2]);
+	
+	path = d3.geo.path().projection(projection);
+	
+	svg = d3.select("#map")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+			.attr("id", "svgBase");
+	
+	d3.json('Datos/MapsGeoJson/Edificios.json', function(data) {
+		var features = data.features;
+//		console.log(features[0]);
+		
+		// Iterates over the data and draws something
+		var baseLayersGrp = svg.append('g').attr('class', 'boundary');
+		
+		baseLayersGrp.selectAll('path').data(features)
+				.enter().append('path')
+				.attr('d', path);
+		//			  .on('mouseover', mouseoverDep);
+		readData();
+	});
 })();
+
+
+function readFile(fileName, dataId){
+	d3.csv(fileName, function(data) {
+
+		dataGeo[dataId] = data;
+		/*
+		dataGeo[attr] = {};
+		dataGeo.bicOne.type = "FeatureCollection";
+		dataGeo.crs = {properties: {name:"urn:ogc:def:crs:OGC:1.3:CRS84"}};
+		dataFeatures = data.map( obj => {
+							var res = {}; 
+							res.type = "Feature";
+							res.properties = {date:obj.date};
+							res.geometry = {type:"Point",
+											coordinates:[obj.lon, obj.lat]};
+							return res;
+						});
+		dataGeo.features = dataFeatures;*/
+
+	// Iterates over the data and draws something
+		var baseLayersGrp = svg.append('g').attr('class', 'bici');
+		
+		baseLayersGrp.selectAll('circle').data(data)
+				.enter().append('circle')
+				.attr('cx', function(d){ return projection([d.lon,d.lat])[0];})
+				.attr('cy', function(d){ return projection([d.lon,d.lat])[1];})
+				.attr('r',1);
+	});
+
+}
+/**
+ * This function should read and plot all the data to display 
+ * @returns {undefined}
+ */
+function readData(){
+//	readFile('Datos/CO/1_bicicleta.csv', 'bici1');
+//	readFile('Datos/CO/2_bicicleta.csv', 'bici2');
+	readFile('Datos/CO/3_bicicleta.csv', 'bici3');
+
+	readFile('Datos/CO/1_caminando.csv', 'cam1');
+}
+
+function resizeMap(width, height){
+	svg.attr("width", width)
+			.attr("height", height);
+	path.projection()
+			.center([-99.1792, 19.32])
+			.translate([width / 2, height / 2]);
+	
+	// Iterates over the data and draws something
+	d3.selectAll('path').attr('d', path);
+	d3.selectAll('circle').attr('d', path);
+}
+
+function mouseoverDep(){
+	console.log("mouseoverDep");
+}
+
+
+$(window).resize(function(){
+	var width = window.innerWidth - 2*margin;
+	var height= window.innerHeight - 2*margin;
+	resizeMap(width, height);
+})
